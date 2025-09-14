eae13a6301816a940968fc658bc9e4470273e87b
aknxad <alikhajoueinejad@gmail.com>
2025-09-14 20:57:50 +0330

    add env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UrlShortenerModule } from './modules/url-shortener/url-shortener.module';
 import { GeneratingUrlShortPath } from './modules/url-path/url-path';
-
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [UrlShortenerModule],
+  imports: [UrlShortenerModule, ConfigModule.forRoot()],
   controllers: [],
   providers: [GeneratingUrlShortPath],
 })
